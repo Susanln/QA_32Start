10d327a2ed4283f56e52ad0a961d33b95c7fbacc
Susanln <susanln@mail.ru>
2022-03-14 13:16:33 +0200

    add

--- src/test/java/tests/Test32_1.java
+++ src/test/java/tests/Test32_1.java
@@ -10,6 +10,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class Test32_1 {
     WebDriver wd;
 
@@ -22,15 +24,54 @@ public class Test32_1 {
         wd.navigate().to("https://trello.com/");
     }
 
-    @Test
+    @Test 
             public void test1() {
         WebElement el = wd.findElement(By.tagName("a"));
         WebElement el2 = wd.findElement(By.tagName("div"));
+        WebElement el22 = wd.findElement(By.cssSelector("div"));
+
+
         WebElement el3 = wd.findElement(By.id("hero"));
         WebElement el4 = wd.findElement(By.cssSelector("#hero"));
+        WebElement e144 = wd.findElement(By.xpath("//*[@id='hero']"));
+
         WebElement el5 = wd.findElement(By.linkText("Sign up"));
         WebElement el6 = wd.findElement(By.cssSelector("[href='/signup']"));
+
+        WebElement e17 = wd.findElement(By.cssSelector("[data-analytics-screen='homeScreen']"));
+        WebElement e18 = wd.findElement(By.cssSelector("[data-analytics-screen *='Screen']"));
+        WebElement e19 = wd.findElement(By.cssSelector("[data-analytics-screen ^='ho']"));
+        WebElement e20 = wd.findElement(By.cssSelector("[data-analytics-screen $='een']"));
+
+        WebElement e21 = wd.findElement(By.xpath("//*[@data-analytics-screen='homeScreen']"));
+        WebElement e22 = wd.findElement(By.xpath("//*[contains(@data-analytics-screen='Screen')]"));
+        WebElement e23 = wd.findElement(By.xpath("//*[start-with(@data-analytics-screen='ho')]"));
+
     }
+    @Test
+    public void test2(){
+        List<WebElement> eln = wd.findElements(By.tagName("a"));
+        System.out.println(eln.size());
+
+    }
+    @Test
+    public void enterPositiveEmail(){
+        WebElement elm = wd.findElement(By.cssSelector("input[name='email']"));
+        type(elm,"susanln@mail.ru");
+
+    }
+
+    public void type(WebElement elm, String text) {
+        elm.click();
+        elm.clear();
+        elm.sendKeys(text);
+    }
+    @Test
+    public void negativeEmail(){
+        WebElement elm = wd.findElement(By.cssSelector("input[name='email']"));
+        type(elm,"susanlnmail.ru");
+    }
+
     @AfterMethod
     public void postConditions(){
         wd.close();
